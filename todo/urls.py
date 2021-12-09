e15520c780e3665c598dbd02c236e470aae43331
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-09 12:56:21 +0000

    add projects path

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('users/actions/', views.ActionList.as_view()),
+    path('users/projects/', views.ProjectList.as_view()),
+    # re_path(r'^users/actions/focused/$', views.FocusedActionList.as_view()),
 ]
